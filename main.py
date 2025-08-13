834cda416b6b31371ca63b0b30891e6f735aa6c6
nrenars <114558077+nrenars@users.noreply.github.com>
2025-08-13 23:29:50 +0300

    a
    a

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 from os import getenv
 from time import sleep
-
+import random
 from discord.ext import commands
 import discord
 
 load_dotenv()
 
-discord_client_secret = getenv("DISCORD_CLIENT_SECRET")
+DISCORD_CLIENT_SECRET = getenv("DISCORD_CLIENT_SECRET")
 CLIENT_ID = getenv("CLIENT_ID")
 CLIENT_SECRET = getenv("CLIENT_SECRET")
 
@@ -23,30 +23,40 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents)
+bot.remove_command('help') 
 
 
-# results = sp.search(q='weezer', limit=20)
-
-# print(results)
-# print("Number of tracks:", len(results['tracks']['items']))
-# for idx, track in enumerate(results['tracks']['items']):
-#     print(idx, track['name'])
-
-# x = sp.user('4cbdkhwgf34wtzfq6pkmwo9ik')
-# print(x)
-# print(x['followers']['total'])
-
-# y = sp.album('https://open.spotify.com/album/1NAmidJlEaVgA3MpcPFYGq?si=Lr8Xb_9qSg6LkkeT2mSOUw')
-# print(y)
-
-# z = sp.add_to_queue("https://open.spotify.com/track/0FIDCNYYjNvPVimz5icugS?si=fab28a48287342c0")
-# print(z)
+HELP_MESSAGE = """
+```Command           | Action                             | Example 
+------------------| -----------------------------------|---------------
+/queue            | Add a song to queue using an url   | /queue https://open.spotify.com/track/xyz
+/squeue           | Add a song to queue by using a     | /squeue Never Gonna Give You Up
+                  | song name                          | 
+/aqueue           | Add a random top track from an     | /aqueue Ed Sheeran
+                  | artist to the queue                |
+/account          | Show account info                  | /account
+/pause            | Pause                              | /pause
+/resume           | Unpause                            | /resume
+/song             | Show info about a currently        | /song
+                  | playing song                       |
+/history          | Show 10 most recently played songs | /history
+/skip             | Skip to the next song in the queue | /skip
+/back             | Play previous song                 | /back
+/volume           | Change the volume of a currently   | /volume 50
+                  | playing song                       | 
+/upcoming         | Show all of the songs that are in  | /upcoming
+                  | the queue                          |
+/help             | Show this help message             | /help
+```
+"""
 
 @bot.event
 async def on_ready():
-    q = sp.search(q='Bunkurs 13',type='track', limit=1)
-    print(q)
+    print("Botify is ready!")
 
+@bot.event
+async def on_guild_join(ctx):
+    await ctx.send(HELP_MESSAGE)
 
 @bot.command()
 async def queue(ctx, url):
@@ -64,9 +74,9 @@ async def queue(ctx, url):
     await ctx.send(embed=embeded)
 
 @bot.command()
-async def squeue(ctx, *, name):
+async def squeue(ctx, *, song_name):
     try:
-        song_data = sp.search(q=name, type='track', limit=1)
+        song_data = sp.search(q=song_name, type='track', limit=1)
         song = song_data['tracks']['items'][0]
         song_uri = song['uri']
         sp.add_to_queue(song_uri)
@@ -79,6 +89,27 @@ async def squeue(ctx, *, name):
     except Exception as e:
         print(e)
     await ctx.send(embed=embeded)
+
+@bot.command()
+async def aqueue(ctx, *, artist_name):
+    try:
+        artist_data = sp.search(q=artist_name, type='artist', limit=1)
+        artist_id = artist_data['artists']['items'][0]['id']
+        top_tracks_data = sp.artist_top_tracks(artist_id)
+        random_track_data = top_tracks_data['tracks'][random.randint(0, top_tracks_data['tracks'])]
+        random_track_uri = random_track_data['uri']
+        sp.add_to_queue(random_track_uri)
+
+        embeded = discord.Embed(
+            title = f"{random_track_data['name']} by {random_track_data['artists'][0]['name']}",
+            description = "Added to queue",
+            color=discord.Color.green())
+        img = random_track_data['album']['images'][0]['url']
+        embeded.set_image(url=img)
+        await ctx.send(embed=embeded)
+    except Exception as e:
+        print(e)
+
 
 @bot.command()
 async def account(ctx):
@@ -98,7 +129,7 @@ async def pause(ctx):
     await ctx.send(f"Paused {song['item']['name']} by {song['item']['artists'][0]['name']}")
 
 @bot.command()
-async def unpause(ctx):
+async def resume(ctx):
     song = sp.currently_playing()
     if song['is_playing']:
         pass
@@ -173,16 +204,17 @@ async def upcoming(ctx):
     songs = sp.queue()
     queue_songs = ""
     for track in songs['queue']:
-        # track = track['track']
         queue_songs += f"{track['name']} by {track['artists'][0]['name']}\n"
 
     embeded = discord.Embed(
         title = "List of songs in the queue:",
         description = queue_songs,
-        color=discord.Color.green()
+        color=discord.Color.green(),
     )
     await ctx.send(embed=embeded)
 
+@bot.command()
+async def help(ctx):
+    await ctx.send(HELP_MESSAGE)
 
-
-bot.run(discord_client_secret)
+bot.run(DISCORD_CLIENT_SECRET)
